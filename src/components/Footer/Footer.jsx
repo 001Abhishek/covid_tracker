f0f57b7f1743c3ad6d0b1e7dc9339d710577d03f
001Abhishek <aprajput321@gmail.com>
2020-12-25 19:37:41 +0530

    Initial commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from './Footer.module.css'
+
+const Footer = () => (
+  <div > 
+        <p className={styles.footer}> Abhishek Pathania &copy; 2020</p>
+
+  </div>
+);
+
+export default Footer;
